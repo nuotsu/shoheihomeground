ced780da152cee0b46b8464201df4b2f862378f1
Mitchell Christ <xzmaru@gmail.com>
2022-08-19 19:27:50 -0700

    remove cloudinary

--- packages/kit/src/lib/Date.svelte
+++ packages/kit/src/lib/Date.svelte
@@ -10,16 +10,4 @@
 		day: 'numeric',
 		year: 'numeric',
 	}) => new Intl.DateTimeFormat('en', options).format(new Date(date + 'T00:00:00.000'))
-
-	export function format_v1(date) {
-		if (!date) return
-
-		const { y, m, d } = date.match(/(?<y>\d{2})-(?<m>\d{2})-(?<d>\d{2})/).groups
-
-		return new Intl.DateTimeFormat('en', {
-			month: 'long',
-			day: 'numeric',
-			year: 'numeric',
-		}).format(new Date('20' + y, m-1, d))
-	}
 </script>
