dd72564756c9b9aae386f2c48b1af722251d9538
Mitchell Christ <xzmaru@gmail.com>
2022-09-05 16:00:52 -0700

    try svelte:head for GA4 tag

--- packages/kit/src/routes/+layout.svelte
+++ packages/kit/src/routes/+layout.svelte
@@ -1,3 +1,16 @@
+<svelte:head>
+	{#if !dev}
+		<!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-HCBHHJC8BK"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+			gtag('config', 'G-HCBHHJC8BK');
+		</script>
+	{/if}
+</svelte:head>
+
 <Header/>
 
 <main>
@@ -9,5 +22,6 @@
 <script>
 	import Header from '$lib/header/Header.svelte'
 	import Footer from '$lib/footer/Footer.svelte'
+	import { dev } from '$app/env'
 	import '../styles/app.css'
 </script>
