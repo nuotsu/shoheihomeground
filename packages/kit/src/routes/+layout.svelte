c573f0620f71a4d25991f9cc2967239dbd204100
Mitchell Christ <xzmaru@gmail.com>
2022-09-06 21:25:10 -0700

    commented dev

--- packages/kit/src/routes/+layout.svelte
+++ packages/kit/src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <svelte:head>
-	{#if !dev}
+	<!-- {#if !dev} -->
 		<!-- Google Tag Manager -->
 		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -7,7 +7,7 @@
 		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 		})(window,document,'script','dataLayer','GTM-MH9ZR87');</script>
 		<!-- End Google Tag Manager -->
-	{/if}
+	<!-- {/if} -->
 </svelte:head>
 
 <Announcement/>
@@ -23,6 +23,6 @@
 	import Announcement from '$lib/header/Announcement.svelte'
 	import Header from '$lib/header/Header.svelte'
 	import Footer from '$lib/footer/Footer.svelte'
-	import { dev } from '$app/env'
+	// import { dev } from '$app/env'
 	import '../styles/app.css'
 </script>
